6f704a7610ed8bde1ae88449a37f00b1c5112b07
Daniil10Sh <danik2022odessa@gmail.com>
2024-04-18 18:53:58 +0300

    add

---
+++ script.js
@@ -0,0 +1,26 @@
+// let num = 1;
+// while(num <= 10){
+//     console.log(num)
+//     num++;
+// }
+ 
+
+// for(let i = 1; i <= 20; i++) {
+//     if(i % 2) {
+//         console.log(i)
+//         continue;
+//     }
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     let result = 7 * i;
+//     console.log(result);
+// }
+
+
+const numbers = [1, 2, 3, 4, 5];
+let i = 0;
+while (i < numbers.length) {
+    console.log(numbers[i]);
+    i++;
+}
